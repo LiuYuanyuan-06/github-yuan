b9b8ad83c72e64faaa32c0a8c346d5f1c5f026be
LiuYuanyuan-06 <2698280960@qq.com>
2026-03-19 20:21:34 +0800

    refactor:optimize lab02 code structure

--- lab02/lab02.ino
+++ lab02/lab02.ino
@@ -1,4 +1,4 @@
-// 定义板载LED引脚
+// 定义板载LED引脚，分别定义三个
 #define LED_PIN 2
 #define LED_PIN_R 26
 #define LED_PIN_1 27
@@ -17,16 +17,16 @@ void setup() {
 void loop() {
   Serial.println("Hello ESP32!");
   digitalWrite(LED_PIN, HIGH); // 点亮LED
-  digitalWrite(LED_PIN_R, HIGH);
-   digitalWrite(LED_PIN_1,HIGH);
-    digitalWrite(LED_PIN_2,HIGH);
-    Serial.println("LED ON");    // 串口输出提示
+   digitalWrite(LED_PIN_R, HIGH);
+    digitalWrite(LED_PIN_1,HIGH);
+     digitalWrite(LED_PIN_2,HIGH);
+  Serial.println("LED ON");    // 串口输出提示
   delay(1000); // 等待1秒
   
   digitalWrite(LED_PIN, LOW);    // 熄灭LED
-digitalWrite(LED_PIN_R, LOW);
-digitalWrite(LED_PIN_1,LOW);
-digitalWrite(LED_PIN_2,LOW);
+   digitalWrite(LED_PIN_R, LOW);
+     digitalWrite(LED_PIN_1,LOW);
+       digitalWrite(LED_PIN_2,LOW);
   Serial.println("LED OFF");   // 串口输出提示
 
   delay(1000);              // 等待1秒
